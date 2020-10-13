baca0bd7f78768ae42dbd0ef24168c28720e69e5
QuantumChord <60354100+QuantumChord@users.noreply.github.com>
2020-10-13 17:57:42 -0400

    Making Use of XML Through Code

---
+++ Assets/__Scripts/Card.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    //Defined later
+}
+
+[System.Serializable]
+
+public class Decorator
+{
+    //This class stores information about deach decorator or pip from DeckXML
+    public string type;
+
+    public Vector3 loc;
+
+    public bool flip = false;
+
+    public float scale = 1f;
+}
+
+[System.Serializable]
+
+public class CardDefinition
+{
+    //This class stores information for each rank of card
+    public string face;
+
+    public int rank;
+
+    public List<Decorator> pips = new List<Decorator>();
+}
