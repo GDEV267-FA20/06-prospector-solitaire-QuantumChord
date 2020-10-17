153bdcc85b771273e10bc351250acb2380a9f858
QuantumChord <60354100+QuantumChord@users.noreply.github.com>
2020-10-17 19:35:34 -0400

    Making Cards Clickable

--- Assets/__Scripts/CardProspector.cs
+++ Assets/__Scripts/CardProspector.cs
@@ -34,4 +34,18 @@ public class CardProspector : Card
     //The SlotDef class stores information pulled in from the LayoutXML <slot>
 
     public SlotDef slotDef;
+
+    //This allows the card to react to being clicked
+
+    public override void OnMouseUpAsButton()
+    {
+
+        //Call the CardClicked method on the Prospector singleton
+
+        Prospector.S.CardClicked(this);
+
+        //Also call the base class (Card.cs) version of this method
+
+        base.OnMouseUpAsButton();
+    }
 }
